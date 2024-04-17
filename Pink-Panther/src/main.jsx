16e4fceda7f114a2836cbf303ba7d01e39acdff3
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-04-17 10:48:19 -0300

    cambio de errores en redux store y main.jsx

--- Pink-Panther/src/main.jsx
+++ Pink-Panther/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import store from './Redux/store.js';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
