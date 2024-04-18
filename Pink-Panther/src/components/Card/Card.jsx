17499e6310e8a1f60aa192ea6c1fbf20f7ea70ca
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-04-18 03:05:30 -0300

    creación Card

--- Pink-Panther/src/components/Card/Card.jsx
+++ Pink-Panther/src/components/Card/Card.jsx
@@ -1,3 +1,23 @@
+// Importa los estilos y las dependencias necesarias
+import style from './Card.module.css';
+import { Link } from 'react-router-dom';
 
-
-export default Card
+// Define y exporta la función del componente Card
+export default function Card({ id, name, categoryName, subcategoryName, color, priceEfectivo, size, quantity, photo}) {
+   return (
+      <div className={style.card}>
+         <h3 className={style.h3}>{name}</h3>
+         <img className={style.image} src={photo} alt={`${name} Photo`} />
+         <h4 className={style.h4}>Category: {categoryName}</h4>
+         <h4 className={style.h4}>Subcategory: {subcategoryName}</h4>
+         <h4 className={style.h4}>Color: {color}</h4>
+         <h4 className={style.h4}>Price Efectivo: {priceEfectivo}</h4>
+         <h4 className={style.h4}>Size: {size}</h4>
+         <h4 className={style.h4}>Quantity: {quantity}</h4>
+         {/* Enlace a la página de detalles del producto */}
+         <Link to={`/detail/${id}`} className={style.link}>
+            Ver detalles
+         </Link>
+      </div>
+   );
+}
